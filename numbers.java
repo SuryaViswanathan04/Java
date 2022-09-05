f77754bb99b017f904bb78390a1190d4d4671899
Surya Viswanathan <106607233+SuryaViswanathan04@users.noreply.github.com>
2022-09-05 07:22:25 -0500

    Add files via upload

---
+++ numbers.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class numbers
+{
+public static void main(String[ ] arg )
+{
+int num1,num2,sum;
+num1=5;
+num2=10;
+sum=num1+num2;
+System.out.println("Sum of the two number is:"+sum);
+}
+}
