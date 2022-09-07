598a345f913a965a217e71f37a01a164ade5187b
Surya Viswanathan <106607233+SuryaViswanathan04@users.noreply.github.com>
2022-09-07 20:36:49 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,14 @@
+import java.io.*;
+ class Ascii
+{
+
+    public static void main(String[] args) 
+{
+
+        char ch = 'a';
+        int ascii = ch;
+        int castAscii = (int) ch;
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
