7276715d79a4838788adb3c8ba08e6dd577b7cb4
Surya Viswanathan <106607233+SuryaViswanathan04@users.noreply.github.com>
2022-09-08 07:44:18 -0500

    Java Program to Compute Quotient and Remainder

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class Main 
+{
+    public static void main(String[] args)
+    {
+ 
+        int dividend = 4567, divisor = 8;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("The Quotient is = " + quotient);
+        System.out.println("The Remainder is = " + remainder);
+    }
+}
